af93c5d2226f7828531c404f21618770b661ded6
caspian <casa04001@utb.vaxjo.se>
2022-09-09 14:32:29 +0200

    Add project files.

---
+++ Uppgift 2.10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_2._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favoritbok");
+
+            string x = Console.ReadLine();
+           Console.WriteLine("Din favoritebok är " + x +"" );
+          
+        }
+
+
+    }
+
+
+}
